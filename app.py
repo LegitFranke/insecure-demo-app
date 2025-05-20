62a6e666ebdc47caf30e858ee9908f7c8d8b4708
Baby Bear <frankie@MacBook-Air.local>
2025-05-20 17:35:51 -0500

    Add medium and low severity secrets

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ slack_webhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXX
 print("Starting app with credentials...")
 # Base64 encoded basic auth (demo only)
 encoded_auth = "dXNlcjpwYXNzd29yZA=="  # base64 for user:password
+SENDGRID_API_KEY = "SG.xxxxxxxx.fakekey5678"
+SLACK_BOT_TOKEN = "xoxb-987654321012-1234567890987-AbCdEfGhIjKlMnOpQrSt"
+DB_PASSWORD = "123456"
+JWT_SECRET = "mydemoJWTsecretkey"
+API_KEY = "abcdefg12345"
